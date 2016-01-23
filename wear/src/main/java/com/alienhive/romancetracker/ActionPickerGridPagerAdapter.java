45e87eff939c848c73bf946e1dba1e8b3d722a03
Steven McWhorter <steven.mcwhorter@gmail.com>
2016-01-23 15:33:20 -0500

    fragment refactor

--- wear/src/main/java/com/alienhive/romancetracker/ActionPickerGridPagerAdapter.java
+++ wear/src/main/java/com/alienhive/romancetracker/ActionPickerGridPagerAdapter.java
@@ -5,57 +5,37 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.support.wearable.view.FragmentGridPagerAdapter;
 
-import com.alienhive.romancetracker.domain.ActionColumn;
 import com.alienhive.romancetracker.domain.ActionPageRow;
 
 import java.util.ArrayList;
 
-public class ActionPickerGridPagerAdapter extends FragmentGridPagerAdapter
-{
+public class ActionPickerGridPagerAdapter extends FragmentGridPagerAdapter {
     private Context context;
-    private FragmentManager fm;
-    private ArrayList<ActionPageRow> actionPageList;
-    private ArrayList<ActionColumn> actionColumnList;
+    ArrayList<ActionPageRow> actionPageList;
 
     public ActionPickerGridPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
         this.context = context;
-        this.fm = fm;
         createActionPages();
     }
 
-    private void createActionPages()
-    {
+    private void createActionPages() {
         actionPageList = new ArrayList<>(2);
-        ActionPageRow row0 = new ActionPageRow("Hugs", "Track big or small hugs.");
-        ActionPageRow row1 = new ActionPageRow("Kisses", "Track big or small kisses.");
-        actionPageList.add(row0);
-        actionPageList.add(row1);
-
-        actionColumnList = new ArrayList<>(2);
-        ActionColumn actionColumn1 = new ActionColumn();
-        actionColumnList.add(actionColumn1);
-        ActionColumn actionColumn2 = new ActionColumn();
-        actionColumnList.add(actionColumn2);
+        ActionPageRow actionRow1 = new ActionPageRow("Hugs", "Track big or small hugs.");
+        ActionPageRow actionRow2 = new ActionPageRow("Kisses", "Track big or small kisses.");
+        actionPageList.add(actionRow1);
+        actionPageList.add(actionRow2);
     }
 
     @Override
     public Fragment getFragment(int row, int col) {
-        if(((row == 0) || (row == 1)) && (col == 0)) {
-            return actionPageList.get(row).getFragment();
-        }
-        else if(((row == 0) || (row == 1)) && (col == 1)) {
-            return actionColumnList.get(row).getFragment();
-        }
-        else
-        {
-            return null;
-        }
+        return actionPageList.get(row).getFragment(col);
+
     }
 
     @Override
     public int getRowCount() {
-        if(actionPageList != null)
+        if (actionPageList != null)
             return actionPageList.size();
         else
             return 0;
@@ -63,6 +43,9 @@ public class ActionPickerGridPagerAdapter extends FragmentGridPagerAdapter
 
     @Override
     public int getColumnCount(int i) {
-            return 2;
+        if (actionPageList != null)
+            return actionPageList.get(i).getColumnCount();
+        else
+            return 0;
     }
 }
--- wear/src/main/java/com/alienhive/romancetracker/domain/ActionPageRow.java
+++ wear/src/main/java/com/alienhive/romancetracker/domain/ActionPageRow.java
@@ -1,21 +1,54 @@
 package com.alienhive.romancetracker.domain;
 
 import android.app.Fragment;
-import android.support.wearable.view.ActionPage;
 
 import com.alienhive.romancetracker.ActionTypeFragment;
 
+import java.util.ArrayList;
+
 public class ActionPageRow {
 
+    //Private fields
     private Fragment actionTypeFragment;
+    private ArrayList<ActionColumn> actionColumnList;
 
-    public Fragment getFragment() {
-        return actionTypeFragment;
-    }
-
+    /**
+     * Constructor
+     * @param title
+     * @param description
+     */
     public ActionPageRow(String title, String description)
     {
-        actionTypeFragment = ActionTypeFragment.newInstance(title, description);
+        setupActionTypeColumn(title, description);
+        setupActionColumns();
+    }
 
+    public Fragment getFragment(int col) {
+        if (col == 0) {
+            return actionTypeFragment;
+        }
+        else if (col >= 1) {
+            return actionColumnList.get(col - 1).getFragment();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public int getColumnCount() {
+        return actionColumnList.size() + 1;
+    }
+
+    private void setupActionTypeColumn(String title, String description) {
+        actionTypeFragment = ActionTypeFragment.newInstance(title, description);
+    }
+
+    private void setupActionColumns() {
+        actionColumnList = new ArrayList<>(2);
+        ActionColumn actionColumn1 = new ActionColumn();
+        actionColumnList.add(actionColumn1);
+        ActionColumn actionColumn2 = new ActionColumn();
+        actionColumnList.add(actionColumn2);
     }
 }
